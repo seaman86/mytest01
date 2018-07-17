fe66bb18c5e9ac2dc6e7334714132e8faf982085
seaman86 <849335444@qq.com>
2018-07-17 09:26:31 +0800

    create project

---
+++ HelloWorld/src/com/seaman/main/TestDemo.java
@@ -0,0 +1,10 @@
+package com.seaman.main;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+
+}
